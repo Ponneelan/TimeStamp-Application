9659f6c78a47b94a62efee02bb2c9a201d61fc12
Ponneelan <ponneelan1305@gmail.com>
2023-05-26 18:16:52 +0530

    time stamp application

--- Server/index.js
+++ Server/index.js
@@ -1,35 +1,34 @@
 const express = require('express');
-const cors = require('cors');
 const mysql = require('mysql');
+const cors = require('cors');
 
-
-const app = express();
-app.use(cors());
 
 const connection = mysql.createConnection({
     host:'localhost',
     database:'Employee',
-    password:'root',
-    user:'root'
-})
+    user:'root',
+    password:'root'
+});
 
 connection.connect();
+
+const app = express();
+app.use(cors());
 
 app.get('/getall',(req,res)=>{
     res.set('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    let query = 'select id,name,mobileNumber,email from Employee where isDeleted = ?';
-    connection.query(query,[0],(error,data)=>{
+    sql = 'select id,name,mobileNumber,email from Employee where isDeleted = ?'
+    connection.query(sql,[0],(error,result)=>{
         if(error){
-            console.error(error);
+            console.warn(error);
         }else{
-            res.json(data);
+            res.json(result);
         }
         res.end();
     })
-})
+});
 
-app.listen(3000,()=>{
-    console.log('app runnig on port 3000');
-})
-
+app.listen(3000, () => {
+    console.log(`Server started on port:3000`);
+});
